d4d33a796f238f2ac3cac15a00d2ac66406d6604
sssslin <414048771@qq.com>
2020-06-09 10:45:32 +0800

    first commit

---
+++ src/main/java/rabbitmq/lessson1/Recv.java
@@ -0,0 +1,36 @@
+package rabbitmq.lessson1;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DeliverCallback;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author junfeng.ling
+ * @date 2020/6/9 10:17
+ * @Description:
+ */
+public class Recv {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
+
+        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        DeliverCallback deliverCallback = (consumerTag, deliver) ->{
+            String message = new String(deliver.getBody(), "UTF-8");
+            System.out.println(" [x] Received '" + message + "'");
+        };
+
+        channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag-> {});
+    }
+}
