981808f54c78b8a012ac57bc42889d909565b9af
sssslin <414048771@qq.com>
2020-06-16 20:49:07 +0800

    commit:publish/subscribe

---
+++ src/main/java/rabbitmq/subscribe/EmitLog.java
@@ -0,0 +1,31 @@
+package rabbitmq.subscribe;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author junfeng.ling
+ * @date 2020/6/16 20:20
+ * @Description:
+ */
+public class EmitLog {
+
+    private static final String EXCHANGE_NAME = "logs";
+
+    public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE_NAME, "fanout");
+            String message = args.length < 1? "info: Hello World!": String.join(" ", args);
+            channel.basicPublish(EXCHANGE_NAME, "", null, message.getBytes("UTF-8"));
+            System.out.println(" [x] Sent '" + message + "'");
+        }
+    }
+}
